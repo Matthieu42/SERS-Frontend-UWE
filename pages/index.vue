75f3c8e58994a06fb32f8f00e45f1a9c50c365be
Corentin Grard <corentingrard@MacBook-Pro-de-Corentin.local>
2019-07-12 12:28:36 +0100

    commit sale

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,79 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        SERS-Frontend-UWE
-      </h1>
-      <h2 class="subtitle">
-        Frontend for SERS
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <b-form
+      @submit="onSubmit"
+      oninput="confPassword.setCustomValidity(confPassword.value != password.value ? 'Passwords do not match.' : "")"
+      >
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="email"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="email"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="name">
+        <b-form-input id="name" v-model="form.name" required placeholder="Enter name"></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-3"
+        v-model="form.password"
+        label="Password:"
+        label-for="password"
+      >
+        <b-form-input id="password" type="password" required onchange="checkPassword" placeholder="Enter password"></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Confirm Password:" label-for="confPassword">
+        <b-form-input id="confPassword" type="password" required placeholder="Confirm password"></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+
+  <p v-if="error">
+    <ul>
+      <li>{{ error }}</li>
+    </ul>
+  </p>
+
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      form: {
+        error: "",
+        email: "",
+        name: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    checkPassword: function(e) {
+      var myRegex = new RegExp("?=.{8,}");
+      if (!myRegex.test(password)) {
+        return this.error = "Password height must be over 8";
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
