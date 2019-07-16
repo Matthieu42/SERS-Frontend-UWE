c6bfbf00b657800fa07c039e8d49c29c48e22cf8
Corentin Grard <corentingrard@MacBook-Pro-de-Corentin.local>
2019-07-16 16:19:26 +0100

    login + register

--- pages/admin/addUser.vue
+++ pages/admin/addUser.vue
@@ -1,14 +1,7 @@
 <template>
   <div>
-    <b-form
-      @submit="onSubmit"
-      oninput="confPassword.setCustomValidity(confPassword.value != password.value ? 'Passwords do not match.' : "")"
-    >
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="email"
-      >
+    <b-form @submit="onSubmit">
+      <b-form-group id="email-group" label="Email address:" label-for="email">
         <b-form-input
           id="email"
           v-model="form.email"
@@ -18,91 +11,130 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Student Name:" label-for="name">
-        <b-form-input id="name" v-model="form.name" required placeholder="Enter name"></b-form-input>
+      <b-form-group id="firstname-group" label="Student First Name:" label-for="firstname">
+        <b-form-input
+          id="firstname"
+          v-model="form.firstName"
+          required
+          placeholder="Enter frist name"
+        ></b-form-input>
       </b-form-group>
 
-      <b-form-group
-        id="input-group-3"
-        v-model="form.password"
-        label="Password:"
-        label-for="password"
-      >
-        <b-form-input id="password" type="password" required minlength="8" new-password placeholder="Enter password" aria-describedby="password-help-block"></b-form-input>
-        <b-form-text id="password-help-block">
-          Your password must be  at least 8 characters long.
-        </b-form-text>
+      <b-form-group id="lastname-group" label="Student Last Name:" label-for="lastname">
+        <b-form-input id="lastname" v-model="form.lastName" required placeholder="Enter last name"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-4" label="Confirm Password:" label-for="confPassword">
-        <b-form-input id="confPassword" type="password" required minlength="8" v-on:input="confPassword" placeholder="Confirm password"></b-form-input>
+      <b-form-group id="address-group" label="Address:" label-for="address">
+        <b-form-input id="address" v-model="form.address" required placeholder="Enter address"></b-form-input>
       </b-form-group>
 
-      <b-form-group 
-        label="Inscription to modules"
-        id="input-group-5"
-        v-bind:key="module.id"
-        v-for="module in modules"
-      >
-        <input type="checkbox" :id="key" :value="key" v-model="form.modules">
-        <label :for="module.title"> {{ module.title }} </label>
+      <b-form-group id="password-group" label="Password:" label-for="password">
+        <b-form-input
+          id="password"
+          type="password"
+          required
+          minlength="8"
+          new-password
+          placeholder="Enter password"
+          v-model="form.password"
+          :state="passwordLength"
+        ></b-form-input>
+        <b-form-invalid-feedback
+          :state="passwordLength"
+        >Your password must be at least 8 characters long.</b-form-invalid-feedback>
+        <b-form-valid-feedback :state="passwordLength">Looks Good.</b-form-valid-feedback>
       </b-form-group>
- 
-     <b-button type="submit" variant="primary">Submit</b-button>
+
+      <b-form-group id="confPassword-group" label="Confirm Password:" label-for="confPassword">
+        <b-form-input
+          id="confPassword"
+          type="password"
+          required
+          minlength="8"
+          v-model="form.confPassword"
+          placeholder="Confirm password"
+        ></b-form-input>
+        <b-form-invalid-feedback :state="passwordMatch">Your passwords doesn't match</b-form-invalid-feedback>
+        <b-form-valid-feedback :state="passwordMatch">Looks Good.</b-form-valid-feedback>
+      </b-form-group>
+
+      <b-form-group label="Inscription to modules" id="module-group">
+        <b-form-checkbox-group
+          id="module-group"
+          v-model="form.modules"
+          :options="modules"
+          name="Inscription to modules"
+        ></b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
   </div>
 </template>
 
 <script>
 export default {
-  data() {                                
+  data() {
     return {
       form: {
         email: "",
-        name: "",
+        firstName: "",
+        lastName: "",
+        address: "",
         password: "",
-        modules: [],
+        confPassword: "",
+        modules: []
       },
       modules: [
-          {
-            id: 1,
-            title: "Module 1",
-            acronym: "M1"
-          },
-          {
-            id: 2,
-            title: "Module 2",
-            acronym: "M2"
-          },
-          {
-            id: 3,
-            title: "Module 3",
-            acronym: "M3"
-          },
-          {
-            id: 4,
-            title: "Module 4",
-            acronym: "M4"
-          },
-        ]
+        {
+          id: 1,
+          text: "Module 1",
+          acronym: "M1"
+        },
+        {
+          id: 2,
+          text: "Module 2",
+          acronym: "M2"
+        },
+        {
+          id: 3,
+          text: "Module 3",
+          acronym: "M3"
+        },
+        {
+          id: 4,
+          text: "Module 4",
+          acronym: "M4"
+        }
+      ]
     };
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      alert(JSON.stringify(this.form));
+      console.log(this.form);
     },
-    confPassword: (confPassword) => {
-      const password=document.getElementById("password").value
-      if(password != confPassword){
-        document.getElementById("confPassword").style.border="red"
-      }else{
-        document.getElementById("confPassword").style.border="none"
+    confPassword: confPassword => {
+      const password = document.getElementById("password").value;
+      if (password != confPassword) {
+        document.getElementById("confPassword").style.border = "red";
+      } else {
+        document.getElementById("confPassword").style.border = "none";
       }
+    }
+  },
+  computed: {
+    passwordLength() {
+      return this.form.password.length > 8;
+    },
+    passwordMatch() {
+      return (
+        this.form.confPassword.length > 8 &&
+        this.form.password === this.form.confPassword
+      );
     }
   }
 };
 </script>
 <style>
-  
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    oui
+  <div id="login">
+    <b-form inline>
+      <label class="sr-only" for="email">Email</label>
+      <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="email" placeholder="Email" v-model="email"></b-input>
+      </b-input-group>
+      <label class="sr-only" for="password">Password</label>
+      <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="password" placeholder="Password" v-model="password"></b-input>
+      </b-input-group>
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+      <b-button variant="primary">Login</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+};
 </script>
 
 <style>
-
+#login{
+  padding-top: 50px;
+}
 </style>
