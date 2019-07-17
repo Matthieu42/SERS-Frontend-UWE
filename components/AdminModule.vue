8bc80c8304215dfa365c2d7035cbf9385fafae2c
Bastien Bellino <bastien.bellino@etu.univ-amu.fr>
2019-07-17 12:28:48 +0200

    add admin component for modules page

---
+++ components/AdminModule.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container class="bv-example-row">
+  <b-row id="app">
+      <b-col>[{{ acronym }}] {{ name }}</b-col>
+  </b-row>
+  <b-row>
+    <b-col id="app" v-if="exams.length > 0">{{ exams[0].name }}, exam date : {{ exams[0].date }}</b-col>
+    <b-col id="app" v-if="exams.length > 1">{{ exams[1].name }}, exam date : {{ exams[1].date }}</b-col>
+    <b-col id="app" v-if="exams.length > 2">{{ exams[2].name }}, exam date : {{ exams[2].date }}</b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  props: ["name", "acronym", "exams"]
+};
+</script>
+
+<style>
+  #app {
+    border-style: solid;
+    border-width: 2px;
+    border-color: hsla(209, 50%, 51%, 0.459);
+      border-radius: 2px;
+  }
+</style>
