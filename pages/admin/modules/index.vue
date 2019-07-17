e45f1660fedbdd1217142de2cbebcde961be5c78
Bastien Bellino <bastien.bellino@etu.univ-amu.fr>
2019-07-17 12:27:16 +0200

    modules page

--- pages/admin/modules/index.vue
+++ pages/admin/modules/index.vue
@@ -1,10 +1,80 @@
 <template>
-  <div>penis</div>
+  <div>
+    <b-list-group>
+      <AdminModule id="app2"
+        v-for="module in user.modules"
+        :key="module.id"
+        :name="module.name"
+        :acronym="module.acronym"
+        :exams="module.exams"
+      />
+    </b-list-group>
+  </div>
 </template>
 
 <script>
-export default {};
+import AdminModule from "~/components/AdminModule";
+
+export default {
+  components: {
+    AdminModule
+  },
+  data() {
+    return {
+      bordered: true,
+      striped: true,
+      user: {
+        username: "BB",
+        modules: [
+          {
+            id: 1,
+            name: "Module 1",
+            acronym: "MD1",
+            exams: 
+              [
+                {
+                  name: "Test1",
+                  date: "15/07/2019"
+                },
+                {
+                  name: "Test2",
+                  date: "15/07/2019"
+                },
+                {
+                  name: "Test3",
+                  date: "15/07/2019"
+                },
+              ]
+          },
+          {
+            id: 1,
+            name: "Module 2",
+            acronym: "MD1",
+            exams: 
+              [
+                {
+                  name: "Test1",
+                  date: "15/07/2019"
+                },
+                {
+                  name: "Test2",
+                  date: "15/07/2019"
+                }
+              ]
+          }
+          
+        ]
+      }
+    }
+  }
+}
+
 </script>
 
 <style>
+
+  #app2 {
+    margin-top: 20px;
+  }
+
 </style>
