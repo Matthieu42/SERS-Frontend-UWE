26d0c68c80c3433a2061f4fe15d54b0931ea45c5
Bastien-Bellino <bastien.bellino@etu.univ-amu.fr>
2019-07-18 13:49:47 +0200

    wallah le css

--- components/AdminModule.vue
+++ components/AdminModule.vue
@@ -1,12 +1,12 @@
 <template>
   <b-container class="bv-example-row">
     <b-row id="app">
-      <b-col>[{{ acronym }}] {{ name }}</b-col>
+      <b-col id="name">[{{ acronym }}] {{ name }}</b-col>
     </b-row>
     <b-row>
-    <b-col id="app" v-if="components.length > 0">{{ components[0].name }}, exam coef : {{ components[0].percentage }}</b-col>
-    <b-col id="app" v-if="components.length > 1">{{ components[1].name }}, exam coef : {{ components[1].percentage }}</b-col>
-    <b-col id="app" v-if="components.length > 2">{{ components[2].name }}, exam coef : {{ components[2].percentage }}</b-col>
+    <b-col id="app2" v-if="components.length > 0">{{ components[0].name }}, exam coef : {{ components[0].percentage }}</b-col>
+    <b-col id="app2" v-if="components.length > 1">{{ components[1].name }}, exam coef : {{ components[1].percentage }}</b-col>
+    <b-col id="app2" v-if="components.length > 2">{{ components[2].name }}, exam coef : {{ components[2].percentage }}</b-col>
     </b-row>
   </b-container>
 </template>
@@ -41,8 +41,17 @@ export default {
 </script>
 
 <style>
+#name {
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
 #app {
   background-color: #bccef6;
   border-radius: 2px;
 }
+#app2 {
+  background-color: #e1e8f9;
+  border-radius: 2px;
+}
+
 </style>
